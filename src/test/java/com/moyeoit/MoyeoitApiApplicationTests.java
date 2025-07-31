aec59b56df50f4c1c3c62e54c400b082ed743403
jin-daram <jindaram1999@gmail.com>
2025-07-31 21:31:58 +0900

    init: project initialize commit

---
+++ src/test/java/com/moyeoit/MoyeoitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyeoit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoyeoitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
