e6d3a10a984bc926a66dcb82cd67f3156c4b0661
HuitaePark <qkrgmlxo3174@gmail.com>
2025-08-06 20:59:01 +0900

    feat: cors 설정 추가 협업을 위해 모든 메서드와 헤더의 모든 필드 허용

--- src/main/java/com/moyeoit/global/config/SecurityConfig.java
+++ src/main/java/com/moyeoit/global/config/SecurityConfig.java
@@ -1,14 +1,35 @@
 package com.moyeoit.global.config;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
+
+    @Bean //cors 설정 빈
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+        config.setExposedHeaders(List.of("*","Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 
 }
