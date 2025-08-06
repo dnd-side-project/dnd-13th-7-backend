9907a02097e7eab68d63ca98e8561161580767be
HuitaePark <qkrgmlxo3174@gmail.com>
2025-08-06 20:53:09 +0900

    feat: SecurityConfig.java 어노테이션 추가 HTTP 요청에 대한 인증과 인가를 처리및 메서드 단위 권한 검사를 가능하게 하여, 특정 메서드에 권한 기반 접근 제어

--- src/main/java/com/moyeoit/global/config/SecurityConfig.java
+++ src/main/java/com/moyeoit/global/config/SecurityConfig.java
@@ -1,4 +1,14 @@
 package com.moyeoit.global.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfig {
+
 }
