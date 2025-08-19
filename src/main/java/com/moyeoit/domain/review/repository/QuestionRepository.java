9c663f6c46f86c333665b55e66baa174da32700d
jin-daram <jindaram1999@gmail.com>
2025-08-20 00:08:47 +0900

    feat: 누락 커밋 업로드

--- src/main/java/com/moyeoit/domain/review/repository/QuestionRepository.java
+++ src/main/java/com/moyeoit/domain/review/repository/QuestionRepository.java
@@ -1,9 +1,16 @@
 package com.moyeoit.domain.review.repository;
 
 import com.moyeoit.domain.review.domain.Question;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query("SELECT q FROM Question q JOIN FETCH q.questionElements WHERE q.id = :questionId")
+    Optional<Question> findByIdWithQuestionElements(@Param("questionId") Long questionId);
+
 }
