95bd9a0e5ba949366aba6f2fc6fba314520279f6
jin-daram <jindaram1999@gmail.com>
2025-08-25 23:23:56 +0900

    hotfix: Activate 버그 수정 (2)
    JsonProperty 설정

--- src/main/java/com/moyeoit/domain/app_user/controller/request/ActivateRequest.java
+++ src/main/java/com/moyeoit/domain/app_user/controller/request/ActivateRequest.java
@@ -1,5 +1,6 @@
 package com.moyeoit.domain.app_user.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +13,23 @@ import lombok.Setter;
 public class ActivateRequest {
 
     private String nickname;
+
+    @JsonProperty("job_id")
     private Long jobId;
+
+    @JsonProperty("is_over_age")
     private boolean isOverAge;
+
+    @JsonProperty("agree_terms_of_service")
     private boolean agreeTermsOfService;
+
+    @JsonProperty("agree_privacy_policy")
     private boolean agreePrivacyPolicy;
+
+    @JsonProperty("agree_marketing_privacy")
     private boolean agreeMarketingPrivacy;
+
+    @JsonProperty("agree_event_notification")
     private boolean agreeEventNotification;
 
 }
