c541bbbbe64404790b7910d86efe423377dcaab1
HuitaePark <qkrgmlxo3174@gmail.com>
2025-08-29 11:38:22 +0900

    chore: swagger 수정

--- src/main/java/com/moyeoit/domain/club/controller/ClubAPI.java
+++ src/main/java/com/moyeoit/domain/club/controller/ClubAPI.java
@@ -11,10 +11,12 @@ import com.moyeoit.global.response.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import java.util.List;
+import java.util.Map;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -44,5 +46,10 @@ public interface ClubAPI {
     ApiResponse<?> getUserSubscribe(
             @Parameter(hidden = true) @CurrentUser AccessUser user,
             @PageableDefault(size = 12, direction = Sort.Direction.DESC) Pageable pageable);
+
+    @Operation(summary = "유저 동아리 구독 사실 확인 API", description = "유저가 동아리를 구독했는지 확인합니다")
+    ApiResponse<?> checkUserSubscription(
+            @Parameter(hidden = true) @CurrentUser AccessUser user,
+            @RequestParam Long clubId);
 
 }
