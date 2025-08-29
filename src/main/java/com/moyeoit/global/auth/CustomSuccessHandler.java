66f1ca47e3a6b27dada0a0b93ed7cb7a951ac348
jin-daram <jindaram1999@gmail.com>
2025-08-29 19:14:36 +0900

    fix: 소셜 로그인 페이지 조회 시 state 값 넘겨서 해당 state 값으로 redirect 하도록 개선

--- src/main/java/com/moyeoit/global/auth/CustomSuccessHandler.java
+++ src/main/java/com/moyeoit/global/auth/CustomSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.moyeoit.global.auth;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moyeoit.global.auth.jwt.JwtIssuer;
 import com.moyeoit.global.auth.user.CustomOAuth2User;
 import com.moyeoit.global.response.user.LoginResponse;
@@ -10,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -19,10 +19,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CustomSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtIssuer jwtIssuer;
-    private final ObjectMapper objectMapper;
 
     private final String REFRESH_TOKEN_KEY = "REF_TOKEN";
 
@@ -31,6 +31,10 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
                                         Authentication authentication) throws IOException, ServletException {
         OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
         CustomOAuth2User user = (CustomOAuth2User) token.getPrincipal();
+
+        // 0) 프론트엔드 Redirect-URI 변환
+        String redirectURI = request.getParameter("state");
+        log.info("Move to '{}' after login.", redirectURI);
 
         // 1) AccessToken 발급
         String accessToken = jwtIssuer.issueAccess(user.getId(), user.getEmail(), user.isActive());
@@ -47,26 +51,22 @@ public class CustomSuccessHandler implements AuthenticationSuccessHandler {
         String refreshToken = jwtIssuer.issueRefresh(user.getId());
         addCookie(response, REFRESH_TOKEN_KEY, refreshToken, 604800);
 
-        String redirectUrl = UriComponentsBuilder.fromUriString(
-                        "http://localhost:3000/oauth-callback/" + user.getProvider().name().toLowerCase())
+        String redirectAddress = UriComponentsBuilder.fromUriString(
+                        redirectURI + user.getProvider().name().toLowerCase())
                 .queryParam("userId", user.getId())
                 .queryParam("active", user.isActive())
                 .queryParam("accessToken", accessToken)
                 .queryParam("expiresIn", 900L)
                 .build().toUriString();
 
-        response.sendRedirect(redirectUrl);
-
-//        response.setStatus(HttpServletResponse.SC_OK);
-//        response.setContentType("application/json;charset=UTF-8");
-//        objectMapper.writeValue(response.getWriter(), loginResponse);
+        response.sendRedirect(redirectAddress);
     }
 
     private void addCookie(HttpServletResponse response, String name, String value, int maxAgeSeconds) {
         // TODO : Secure 설정
         ResponseCookie.ResponseCookieBuilder b = ResponseCookie.from(name, value)
                 .httpOnly(true)
-                .secure(false)
+                .secure(true)
                 .path("/")
                 .maxAge(Duration.ofSeconds(maxAgeSeconds))
                 .sameSite("Lax");
