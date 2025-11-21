b23bec6d6754f3d4e03a1227c3587a32f7cfbd92
jin-daram <jindaram1999@gmail.com>
2025-11-21 20:30:40 +0900

    fix: Swagger 문서 한글 설명 추가
    ReviewCommentController, AuthController

---
+++ src/main/java/com/moyeoit/domain/auth/presentation/AuthAPI.java
@@ -0,0 +1,24 @@
+package com.moyeoit.domain.auth.presentation;
+
+import com.moyeoit.domain.auth.presentation.request.AuthRequest;
+import com.moyeoit.domain.auth.presentation.response.AuthResponse;
+import com.moyeoit.domain.auth.presentation.response.AuthorizationUriResponse;
+import com.moyeoit.domain.user.domain.AuthProvider;
+import com.moyeoit.global.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public interface AuthAPI {
+
+    @Operation(summary = "인증 URL 조회", description = "OAuth Provider에 맞는 인증 URL을 조회합니다.")
+    ResponseEntity<ApiResponse<AuthorizationUriResponse>> authorize(@RequestParam(name = "redirect_uri") String redirectUri,
+                                                                    @RequestParam String state,
+                                                                    @PathVariable AuthProvider provider);
+
+    @Operation(summary = "로그인 API", description = "OAuth Provider로 부터 발급받은 authentication_code를 통해 로그인을 시도합니다.")
+    ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request);
+
+}
