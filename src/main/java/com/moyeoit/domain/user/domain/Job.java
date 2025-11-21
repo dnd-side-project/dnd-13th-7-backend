dbef0358d5047ca731404727ba5eb56d93b55470
jin-daram <jindaram1999@gmail.com>
2025-11-21 19:45:29 +0900

    fix: Flyway 마이그레이션 엔티티 수정 및 기존 DDL Deprecated

--- src/main/java/com/moyeoit/domain/user/domain/Job.java
+++ src/main/java/com/moyeoit/domain/user/domain/Job.java
@@ -16,6 +16,7 @@ public class Job {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "job_id")
     private Long id;
 
     @Column(name = "name", nullable = false)
