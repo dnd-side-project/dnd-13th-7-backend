41f35c84e2c24daa86ad95fb85e9d953b7c7cd5f
HuitaePark <qkrgmlxo3174@gmail.com>
2026-01-23 01:03:35 +0900

    fix: jwt필터 cors 문제 수정

--- src/main/java/com/moyeoit/global/auth/jwt/JwtFilter.java
+++ src/main/java/com/moyeoit/global/auth/jwt/JwtFilter.java
@@ -34,6 +34,11 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())){
+            filterChain.doFilter(request,response);
+            return;
+        }
+
         String bearer = request.getHeader(AUTHORIZATION_HEADER);
         String token = (StringUtils.hasText(bearer) && bearer.startsWith(BEARER)) ? bearer.substring(7) : null;
         if (token != null && SecurityContextHolder.getContext().getAuthentication() == null) {
